a16ece5ff1d41aa6619cf200d84546c36a80589f
FerArellanoQuijada <farellanoquijad4112@conestogac.on.ca>
2022-12-09 19:12:19 -0500

    PseudoCode and more
    Creation first Play and Class

---
+++ Final Project.py
@@ -0,0 +1,21 @@
+#Author: Fernando Arellano
+#Creation Date: 2022/12/09 
+#Modification Date
+#Game: MineSweeper 
+#Descrition:
+
+
+#Create a board object to repreent the minesweeper game 
+
+class Board: 
+    def __init__(self, dimSize):
+
+
+
+def play (dimSize = 10, NumBombs = 10):
+    #Step 1: Create the board
+    #Step 2: Plant the bombs 
+    #Step 3: Show board to select
+    #step 3a: Show the board repeated times until the user find bombs 
+    #Step 3b: If the user find a bomb, show game over message
+    #step 4: Repeat steps until there is no more spaces or places to look up. 
